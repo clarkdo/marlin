d65d6c04f0375401bc14a70d60db719caaede84c
clarkdo <clark.duxin@gmail.com>
2017-07-26 12:17:52 +0800

    initialize server entry

---
+++ src/index.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+require('babel-register')()
+require('babel-polyfill')
+
+const log = require('fancy-log')
+const server = require('./server').default
+
+const port = process.env.PORT || 5000
+server.listen(port, () => log(`API server started on ${port}`))
---
+++ src/server.js
@@ -0,0 +1,29 @@
+// @flow
+
+import Koa from 'koa'
+import convert from 'koa-convert'
+import logger from 'koa-logger'
+import cors from 'kcors'
+import jwt from 'koa-jwt'
+import bodyParser from 'koa-bodyparser'
+import passport from './passport'
+
+const app = new Koa()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger())
+}
+app.use(convert(cors({ credentials: true })))
+app.use(bodyParser())
+app.use(passport.initialize())
+
+// Parse Authorization Header for JWT tokens, and set ctx.state.user if token is
+// valid. Passthrough to middleware to make decisions on whether or not their
+// routes require users. See src/middleware/validate-user.js
+app.use(jwt({ secret: process.env.APP_SECRET, passthrough: true }))
+
+// Custom API modules that define their own routes.
+const modules = require('./modules')
+modules(app)
+
+export default app
