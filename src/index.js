34fca55a7052d1cf89bf276856e2250966e55687
clarkdo <clark.duxin@gmail.com>
2017-08-14 22:19:06 +0800

    add chalk after startup

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ require('dotenv').config()
 require('babel-register')()
 require('babel-polyfill')
 
-const log = require('fancy-log')
+const chalk = require('chalk')
 const server = require('./server').default
 
 const port = process.env.PORT || 5000
-server.listen(port, () => log(`API server started on ${port}`))
+server.listen(port, () => console.log(chalk.bold(
+  chalk.bgBlue.black('\n OPEN ') +
+  chalk.blue(` http://localhost:${port}/graphql\n`)
+)))
